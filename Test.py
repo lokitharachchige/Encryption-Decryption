50a7f0c8b292b7ac150ace20b82f3793ac6b686d
lokitharachchige <galokith@gmail.com>
2024-06-30 07:46:16 +0530

    Final

--- Test.py
+++ Test.py
@@ -15,3 +15,19 @@ for letter in plain_text:
     index = chars.index(letter)
     cipher_text += key[index]
 
+print(f"original message : {plain_text}")
+print(f"encrypted message: {cipher_text}")
+
+#DECRYPT
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message : {plain_text}")    
+
+
+
